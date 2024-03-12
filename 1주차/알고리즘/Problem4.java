84a579095b55313d25cf1d51088c88e4d5a772a2
hyejin2234 <144921254+hyejin2234@users.noreply.github.com>
2024-03-12 17:46:08 +0900

    ✨ feat: 알고리즘 문제4 코드 작성

---
+++ 1주차/알고리즘/Problem4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class sortinside {
+	public static void main(String[] args) {
+		int a = 0;
+		int[] b = new int[10];
+		
+		Scanner s = new Scanner(System.in);
+		a = s.nextInt();
+		
+		int v = 0;
+		while(a>0) {
+			b[v] = a%10;
+			a = a/10;
+			v++;
+		}
+		int[] bq = new int[v];
+		for(int i=0; i<v; i++) {
+			bq[i]=b[i];
+		}
+		Arrays.sort(bq);
+		for(int i=v-1; i>=0; i--) {
+			System.out.print(bq[i]);
+		}
+	}
+}
